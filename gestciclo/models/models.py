2926716387be0ecbe4ae399b76ebe1e1096b0477
Juan M. Cardeso <a18juancg@iessanclemente.net>
2020-03-29 13:47:45 +0200

    ciclo-modulo-profesor bastante listo

--- gestciclo/models/models.py
+++ gestciclo/models/models.py
@@ -5,8 +5,9 @@ from odoo import models, fields, api
 class Ciclo(models.Model):
     _name = 'gestciclo.ciclo'
     _description = 'Ciclo'
+    _rec_name = 'nombre_ciclo'
 
-    name = fields.Char(required = True)
+    nombre_ciclo = fields.Char(required = True)
     descripcion = fields.Text()
     modulos_ids = fields.One2many('gestciclo.modulo', inverse_name = 'ciclo_id')
 
@@ -37,9 +38,15 @@ class Profesor(models.Model):
 
     partner_id = fields.Many2one('res.partner', ondelete = 'cascade')
     modsprof_ids = fields.One2many('gestciclo.modsprof', inverse_name = 'profesor_id')
-    nif = fields.Char()
+    nif = fields.Char(required = True)
     fecha_inicio = fields.Date('Fecha inicio')
     fecha_fin = fields.Date('Fecha fin')
+
+    @api.constrains('nif')
+    def _check_name(self):
+        partner_rec = self.env['gestciclo.profesor'].search([('nif', '=', self.nif), ('id', '!=', self.id)])
+        if partner_rec:
+            raise ValueError(('El NIF ya existe'))
 
 class ModulosProfesor(models.Model):
     _name = 'gestciclo.modsprof'
@@ -49,17 +56,23 @@ class ModulosProfesor(models.Model):
     modulo_id = fields.Many2one('gestciclo.modulo', ondelete = 'cascade')
     profesor_id = fields.Many2one('gestciclo.profesor', ondelete = 'cascade')
     curso = fields.Char()
+    ciclo = fields.Char('Ciclo', related = 'modulo_id.ciclo_id.nombre_ciclo')
 
 class Alumno(models.Model):
     _name = 'gestciclo.alumno'
     _inherits = {'res.partner': 'partner_id'}
 
     partner_id = fields.Many2one('res.partner', ondelete='cascade')
+    nif = fields.Char(required = True)
     fecha_nacimiento = fields.Date('Fecha de nacimiento')
     fecha_inicio = fields.Date('Fecha inicio')
     fecha_fin = fields.Date('Fecha fin')
 
-
+    @api.constrains('nif')
+    def _check_name(self):
+        partner_rec = self.env['gestciclo.alumno'].search([('nif', '=', self.nif), ('id', '!=', self.id)])
+        if partner_rec:
+            raise ValueError(('El NIF ya existe'))
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
